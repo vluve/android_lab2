924f81f0bd9606650f5d0a9a5fe5514a187cdd13
vluve <vladislav.naidyonov@yandex.ru>
2023-04-06 17:02:51 +0300

    recycler_view_item added
    ADDED: recycler_view_item layout (not styled)
    ADDED: preview images for 50 MMO games
    ADDED: preview info for 50 MMO games
    
    REMOVED: all test files

--- app/src/main/java/com/vluve/mmogames/DetailActivity.java
+++ app/src/main/java/com/vluve/mmogames/DetailActivity.java
@@ -9,6 +9,6 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/com/vluve/mmogames/MainActivity.java
+++ app/src/main/java/com/vluve/mmogames/MainActivity.java
@@ -1,6 +1,8 @@
 package com.vluve.mmogames;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] courses = { "Программирование", "СД", "Матлогика",
+                "Мат. анализ", "Алгебра", "Дискретная математика", "ТВП",
+                "ТЯП", "ООП", "ФЛП", "Криптография", "Экономика",
+                "Культурология", "ТРПО", "СППР", "Сети ЭВМ", "ОС",
+                "Параллельное программирование", "Компьютерная графика" };
+        CoursesAdapter adapter = new CoursesAdapter(courses);
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
